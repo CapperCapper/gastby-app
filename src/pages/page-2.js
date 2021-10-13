0ac4da74cd42102c0b7025ab33563c4e3375ed77
Grace Capper <grace.capper@empyreandigital.com>
2021-10-13 16:15:53 +0100

    Add another API

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,13 +4,60 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
-    <Seo title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+class SecondPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { value: "" }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value })
+  }
+
+  handleSubmit(event) {
+    fetch("/api/contactForm", {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+
+      //make sure to serialize your JSON body
+      body: JSON.stringify({
+        name: this.state.value,
+      }),
+    }).then(response => {
+      console.log(response)
+      //do something awesome that makes the world a better place
+    })
+
+    event.preventDefault()
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Seo title="Page two" />
+        <h1>Hi from the second page</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <p>Welcome to page 2</p>
+        <Link to="/">Go back to the homepage</Link>
+      </Layout>
+    )
+  }
+}
 
 export default SecondPage
